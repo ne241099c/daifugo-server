bbd775dfe89c012ec24701dd6e7268d8a10ea9b7
ne241099c <ne241099@senshu-u.jp>
2026-07-21 20:44:44 +0900

    ログイン消しとばし

--- internal/auth/authenticator.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "context"
-
-// Authenticator はトークンを検証してユーザーIDを特定する責務を持つ
-type Authenticator interface {
-	VerifyToken(ctx context.Context, token string) (int64, int, error)
-	CreateToken(ctx context.Context, userID int64, version int) (string, error)
-}
